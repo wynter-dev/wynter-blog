2cb824d3160a3595967f94c4f8696b798aef3833
wynter <u0lee.dev@gmail.com>
2025-11-27 10:44:04 +0900

    Feat: ga 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import { Metadata } from 'next';
 import '@/styles/globals.css';
+import Script from 'next/script';
 
 const DEFAULT_SITE_URL = 'http://localhost:3000';
 const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || DEFAULT_SITE_URL;
@@ -42,8 +43,31 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: ReactNode}) {
   return (
-    <html lang="ko" suppressHydrationWarning>
+    <html lang="ko">
+    <head>
+      {/* GTM Head Script */}
+      <Script id="gtm-head" strategy="afterInteractive">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MN4J5NJN');
+          `}
+      </Script>
+    </head>
+
     <body>
+    {/* GTM noscript */}
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-MN4J5NJN"
+        height="0"
+        width="0"
+        style={{display: 'none', visibility: 'hidden'}}
+      />
+    </noscript>
+
     {children}
     </body>
     </html>
